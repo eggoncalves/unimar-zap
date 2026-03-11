7e8736f3552358a48f92dc9c7c8555fbab507e87
eggoncalves <161093817+eggoncalves@users.noreply.github.com>
2026-03-11 10:45:19 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAPMHegYnvK_juyF4WmmNu81M5QJ7VnHzY",
-    authDomain: "unimarzapaovivo.firebaseapp.com",
-    projectId: "unimarzapaovivo",
-    storageBucket: "unimarzapaovivo.firebasestorage.app",
-    messagingSenderId: "940207784666",
-    appId: "1:940207784666:web:de54ccec3e0d1a205e9be3"
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_AUTH_DOMAIN",
+    projectId: "SEU_PROJETCT_ID",
+    storageBucket: "SEU_STORAGE_BUCKET",
+    messagingSenderId: "SUA_MESSAGING_SENDER_ID",
+    appId: "SEU_APP_ID"
   },
 };
